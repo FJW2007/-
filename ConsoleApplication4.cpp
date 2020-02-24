1c3cdd77d802ffe86f7052e5d1d43ec8b5bddf6e
FJW2007 <60533328+FJW2007@users.noreply.github.com>
2020-02-24 20:05:07 +0800

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿#include <windows.h>
+#include <iostream>
+#include<Mmsystem.h>
+#pragma comment(lib,"winmm.lib")
+#define KEY_DOWN(VK_NONAME) ((GetAsyncKeyState(VK_NONAME) & 0x8000) ? 1:0)
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+using namespace std;
+int main()
+{A:
+	if (KEY_DOWN(VK_LBUTTON)) {
+		PlaySound(TEXT("c.wav"), NULL, SND_ASYNC | SND_NODEFAULT);
+		for (;;) {
+			MessageBox(NULL, TEXT("Да здравствует великий Советский Союз"), TEXT("Ypa!"), MB_OK | MB_ICONEXCLAMATION);
+			WM_CLOSE;
+		}
+
+	}
+	else {
+	
+	
+		goto A;
+	}
+
+}
